8a58622cc98e8b881bc0ad9894ffbbb3a33de6ae
leungkingto <jingtao.820@163.com>
2018-12-11 17:30:54 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../pages/home'
+import GoodDetail from '../pages/goodDetail'
 
 Vue.use(Router)
 
+const routes = [
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	}, {
+		path: '/goodDetail',
+		name: 'GoodDetail',
+		component: GoodDetail
+	}
+];
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+	routes
 })
