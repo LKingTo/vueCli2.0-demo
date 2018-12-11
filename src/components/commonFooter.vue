d58081da62cfc1d0433dc3542621c578a4d03cac
leungkingto <jingtao.820@163.com>
2018-12-11 15:25:35 +0800

    commonFooter.vue、homeHeader.vue

--- src/components/commonFooter.vue
+++ src/components/commonFooter.vue
@@ -1,5 +1,7 @@
-<template>
-	
+<template lang="pug">
+	.footer
+		ul.footer-cont
+			li(v-for="(item, $index) in 4") {{$index + 1}}
 </template>
 
 <script>
@@ -8,6 +10,29 @@
 	}
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+	.footer
+		height 1.8rem
+		width 100%
+		position fixed
+		bottom 0
+		left 0
+		border-top 2px solid #ccc
+		background-color #fff
+		.footer-cont li
+			float left
+			width 25%
+			height 1.8rem
+			line-height 1.8rem
+			font-size 1.4rem
+			color #ff8000
+			text-align center
+			&:active
+				background-color #ccc
+			&:after
+				content ''
+				display block
+				clear both
+				width 0
+				height 0
 </style>
--- src/components/homeHeader.vue
+++ src/components/homeHeader.vue
@@ -1,5 +1,7 @@
-<template>
-	
+<template lang="pug">
+	.head
+		.header
+			h4.header-cont 主页
 </template>
 
 <script>
@@ -8,6 +10,21 @@
 	}
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+	.head
+		width 100%
+		height 2rem
+		.header
+			width 100%
+			height 2rem
+			position fixed
+			left 0
+			top 0
+			background-color #fff
+			border-bottom 2px solid #ff8000
+			h4.header-cont
+				width 100%
+				text-align center
+				line-height 2rem
+				font-size 1.4rem
 </style>
