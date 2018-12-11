e03e437248e096b9631fad59a4b35edf3ae14eb4
leungkingto <jingtao.820@163.com>
2018-12-11 16:48:58 +0800

    goodDetail.vue

--- src/components/detailHeader.vue
+++ src/components/detailHeader.vue
@@ -1,13 +1,31 @@
-<template>
-	
+<template lang="pug">
+	.head
+		.header
+			h4.header-cont {{ title }}
 </template>
 
 <script>
 	export default {
-		name: "detail-header"
+		name: "detail-header",
+		props: ['title']
 	}
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+	.head
+		width 100%
+		height 2rem
+		.header
+			width 100%
+			height 2rem
+			position fixed
+			left 0
+			top 0
+			background-color #fff
+			border-bottom 2px solid #ff8000
+			h4.header-cont
+				width 100%
+				text-align center
+				line-height 2rem
+				font-size 1.4rem
 </style>
