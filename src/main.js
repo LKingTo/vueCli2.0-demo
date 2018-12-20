64a056df13fda3c2a801521d7d4a5e4a0585f2c2
leungkingto <jingtao.820@163.com>
2018-12-20 18:23:45 +0800

    [Axios]设置全局拦截器

--- src/main.js
+++ src/main.js
@@ -23,6 +23,25 @@ Vue.use(VueResource)
 // Vue.prototype.$axios = axios;  //$http键名已被vue-resource占用
 /** 修改Axios实例默认配置 end **/
 
+/** 设置Axios拦截器 start **/
+//请求拦截器
+Axios.interceptors.request.use((config) => {
+	// console.log('Interceptors request:', config);
+	return config;
+}, (error) => {
+	return Promise.reject(error);
+});
+
+//响应拦截器
+Axios.interceptors.response.use((response) => {
+	// console.log('Interceptors response:', response);
+	//只返回响应数据data
+	return response.data;
+}, (error) => {
+	return Promise.reject(error);
+});
+/** 设置Axios拦截器 end **/
+
 Vue.prototype.$axios = Axios;  //$http键名已被vue-resource占用
 
 Vue.config.productionTip = false
